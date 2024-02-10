06c995dc97c5b655f76cbfcdff9a0ab32294af98
Christian Dale <114800018+saberdale03@users.noreply.github.com>
2024-02-10 22:49:18 +0800

    added transition to nav open and close

--- public/index.js
+++ public/index.js
@@ -1,10 +1,7 @@
-const burger = document.querySelector('#burger');
-const menu = document.querySelector('#menu');
+const burger = document.getElementById('burger');
+const menu = document.getElementById('menu');
 
 burger.addEventListener('click', () => {
-  if(menu.classList.contains('hidden')) {
-    menu.classList.remove('hidden');
-  } else {
-    menu.classList.add('hidden');
-  }
-})
+  menu.classList.toggle('nav-open');
+  menu.classList.toggle('nav-hide');
+});
